c4d96a2cfdf611526b7587f3a253f1e4cf79050f
IB002 <132930816+IB002@users.noreply.github.com>
2023-06-22 14:13:27 -0400

    feat : added functionality to the message

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,14 +1,45 @@
 import { Component, Input } from '@angular/core';
+import {fadeOut,fadeIn, slideOut} from './animations'
 
 @Component({
     selector: 'app-message',
     templateUrl: './message.component.html',
-    styleUrls: ['./message.component.scss']
+    styleUrls: ['./message.component.scss'],
+    animations: [fadeIn, fadeOut,slideOut]
 })
 export class MessageComponent {
+    
+    @Input() gate: string = '';
+    isMessageRemoved = false;
+
+
+    //message
+    clickedMessage:boolean = false;
+    messageDesign:"Information" | "Negative" | "Positive" | "Warning" | undefined = "Information";
+    onClickMessageText:string = "You have checked-in for your flight ";
+    emphasizedOnClickMessage:string = "Download boarding pass."
+
+
+
+
+
     constructor() { }
 
     ngOnInit() { }
 
-    @Input() gate: string = '';
+ 
+
+    closeMessage(){
+        this.isMessageRemoved=!this.isMessageRemoved;
+    }
+    onMessageClick(){
+        if(this.clickedMessage === false){
+        this.clickedMessage = !this.clickedMessage;
+        setTimeout(() => {
+            this.messageDesign = "Positive";
+            this.onClickMessageText = this.onClickMessageText + this.gate + ". ";   
+        },200);
+        }
+    }
+
 }
