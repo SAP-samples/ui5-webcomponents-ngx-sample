5e7a12a59e6b50e4797c0ae0f73fbd3d6ef26a83
IB002 <132930816+IB002@users.noreply.github.com>
2023-07-07 16:25:29 -0400

    chore: replaced custom functionality with carousel

--- src/app/explore-montreal/explore-montreal.component.ts
+++ src/app/explore-montreal/explore-montreal.component.ts
@@ -7,45 +7,4 @@ import { fadeIn } from "./animations";
   styleUrls: ["./explore-montreal.component.scss"],
   animations: [fadeIn],
 })
-export class ExploreMontrealComponent {
-  picCount = 3;
-  currentImageIndex = 1;
-  picAddress = "assets/images/places/montreal_";
-  fullAddress = this.picAddress + this.currentImageIndex + ".png";
-
-  circleArr: number[] = [];
-
-  constructor() {
-    for (let i = 0; i < this.picCount; i++) {
-      this.circleArr.push(i + 1);
-    }
-  }
-
-  getPic() {
-    return this.picAddress + this.currentImageIndex + ".png";
-  }
-
-  nextPic() {
-    if (this.currentImageIndex < 3) {
-      this.currentImageIndex++;
-    } else {
-      this.currentImageIndex = 1;
-    }
-    this.fullAddress = this.getPic();
-  }
-  previousPic() {
-    if (this.currentImageIndex > 1) {
-      this.currentImageIndex--;
-    } else {
-      this.currentImageIndex = this.picCount;
-    }
-    this.fullAddress = this.getPic();
-  }
-
-  getCircle(index: number) {
-    if (index === this.currentImageIndex) {
-      return "circle--blue";
-    }
-    return "circle";
-  }
-}
+export class ExploreMontrealComponent {}
