0ff3fe8e27e3f7897d4c2af9b862ad2f7358da3d
IB002 <132930816+IB002@users.noreply.github.com>
2023-08-18 10:14:51 -0400

    feat: added explore montreal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { PassengerListComponent } from './passenger-list/passenger-list.componen
 import { SeatsChartComponent } from './seats-chart/seats-chart.component';
 import { PaymentDetailsComponent } from './payment-details/payment-details.component';
 import { TripCalendarComponent } from './trip-calendar/trip-calendar.component';
+import { ExploreMontrealComponent } from './explore-montreal/explore-montreal.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { TripCalendarComponent } from './trip-calendar/trip-calendar.component';
     PassengerListComponent,
     SeatsChartComponent,
     PaymentDetailsComponent,
-    TripCalendarComponent
+    TripCalendarComponent,
+    ExploreMontrealComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/explore-montreal/explore-montreal.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-explore-montreal",
+  templateUrl: "./explore-montreal.component.html",
+  styleUrls: ["./explore-montreal.component.scss"],
+})
+
+export class ExploreMontrealComponent {}
